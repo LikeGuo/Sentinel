f7db13e796f9b5ff0f40af4ee6e8922179a75ba3
guoxi <824061269@qq.com>
2022-04-19 11:06:15 +0800

    流量防护控制台代码整体提交

---
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/FlowRuleNacosPublisher.java
@@ -0,0 +1,31 @@
+package com.alibaba.csp.sentinel.dashboard.rule.nacos;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.FlowRuleEntity;
+import com.alibaba.csp.sentinel.dashboard.rule.DynamicRulePublisher;
+import com.alibaba.csp.sentinel.datasource.Converter;
+import com.alibaba.csp.sentinel.util.AssertUtil;
+import com.alibaba.nacos.api.config.ConfigService;
+
+@Component("flowRuleNacosPublisher")
+public class FlowRuleNacosPublisher implements DynamicRulePublisher<List<FlowRuleEntity>> {
+
+	@Autowired
+    private ConfigService configService;
+    @Autowired
+    private Converter<List<FlowRuleEntity>, String> converter;
+
+    @Override
+    public void publish(String app, List<FlowRuleEntity> rules) throws Exception {
+        AssertUtil.notEmpty(app, "app name cannot be empty");
+        if (rules == null) {
+            return;
+        }
+        configService.publishConfig(app + NacosConfigUtil.FLOW_DATA_ID_POSTFIX,
+        		NacosConfigUtil.GROUP_ID, converter.convert(rules));
+    }
+}
